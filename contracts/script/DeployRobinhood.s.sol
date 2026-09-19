445966672b196c3f7e3ab0b3bf59f5e4797eee5f
ozkite <ozkitec@gmail.com>
2026-09-19 12:03:21 +0800

    fix: add valid SPDX license to DeployRobinhood.s.sol

--- contracts/script/DeployRobinhood.s.sol
+++ contracts/script/DeployRobinhood.s.sol
@@ -1,1 +1,19 @@
-// SPDX-License-Identifier: MIT\npragma solidity ^0.8.20;\nimport "forge-std/Script.sol";\nimport "../src/AgentRegistry.sol";\nimport "../src/RoboticsMarketplace.sol";\ncontract DeployRobinhoodScript is Script {\n    function run() external {\n        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");\n        vm.startBroadcast(deployerPrivateKey);\n        AgentRegistry agentRegistry = new AgentRegistry();\n        address mockUsdc = 0x0000000000000000000000000000000000000001;\n        RoboticsMarketplace marketplace = new RoboticsMarketplace(mockUsdc, address(agentRegistry));\n        vm.stopBroadcast();\n        console.log("RH AgentRegistry:", address(agentRegistry));\n        console.log("RH Marketplace:", address(marketplace));\n    }\n}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/AgentRegistry.sol";
+import "../src/RoboticsMarketplace.sol";
+
+contract DeployRobinhoodScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        AgentRegistry agentRegistry = new AgentRegistry();
+        address mockUsdc = 0x0000000000000000000000000000000000000001;
+        RoboticsMarketplace marketplace = new RoboticsMarketplace(mockUsdc, address(agentRegistry));
+        vm.stopBroadcast();
+        console.log("RH AgentRegistry:", address(agentRegistry));
+        console.log("RH Marketplace:", address(marketplace));
+    }
+}
